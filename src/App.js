2f353a80cc1a505482a7b9da7796f8d9bbbd2e39
Alyona <104214804+bordun2022@users.noreply.github.com>
2023-09-07 13:31:18 +0300

    change styles

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import cls from './App.module.css'
 function App() {
   return (
     <>
-    <div className={cls.context} id={cls.context}>
-    <img src={photo} alt={'Something'} className={cls.image} id={cls.image} ></img>
-     <span className={cls.text} id={cls.text}><h2>Hi, I’m Alyona —  beginner frontend web-developer.</h2>
+    <div className={cls.context} >
+    <img src={photo} alt={'Something'} className={cls.image} ></img>
+     <span className={cls.text} ><h2>Hi, I’m Alyona —  beginner frontend web-developer.</h2>
      <p>I value accountability, persistence and honesty above all else. These postulates are my approach to solving life's problems in general. I like writing code and seeing the result. I improve my knowledge daily, keep it up to date and aspire to become a part of the large universe of developers.</p></span>
      </div>
     </>
--- src/pages/MainNavigation.js
+++ src/pages/MainNavigation.js
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 
+
 function MainNavigation() {
   return (
     <header className={classes.header}>
